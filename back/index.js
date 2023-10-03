5f0e166693471f97949f1e37573cd7ef05fe786f
dorapon48 <dorapon48@icloud.com>
2023-10-03 18:39:00 +0900

    update: post処理を追加

--- back/index.js
+++ back/index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// jsonをエンコード
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 // getメソッドで、ルートパスにアクセスしてきたときの処理を記載する
 app.get("/", (req, res) => {
     res.status(200).send({test:"test"});
 });
 
-app.get("/hello", (req, res) => {
-    res.status(200).send("Hello World2");
-});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send("Received POST Data!");
+  });
+
+// app.get("/hello", (req, res) => {
+//     res.status(200).send("Hello World2");
+// });
 
 // リクエストを待ち受ける
 app.listen(8080, ()=>{
